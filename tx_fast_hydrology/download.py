3f6331ed1bff8bf7db8c6a3d862108c566c153fb
Matt Bartos <mdbartos@utexas.edu>
2024-10-25 02:16:20 -0500

    Update download functions

--- tx_fast_hydrology/download.py
+++ tx_fast_hydrology/download.py
@@ -6,13 +6,11 @@ import xarray as xr
 import pandas as pd
 from bs4 import BeautifulSoup
 
-baseurl = 'https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod'
-
-def download_forcings(date_str, forecast_start_hour, comids, sleeptime=0.1):
+def download_forcings(nwm_dir, forecast_start_hour, comids, sleeptime=0.1):
     # Download NetCDF forcings
     datasets = {}
     for lookahead_hour in range(1, 19):
-        url = f'{baseurl}/nwm.{date}/short_range/nwm.t{forecast_start_hour:02}z.short_range.channel_rt.f{lookahead_hour:03}.conus.nc'
+        url = f'{nwm_dir}/short_range/nwm.t{forecast_start_hour:02}z.short_range.channel_rt.f{lookahead_hour:03}.conus.nc'
         response = requests.get(url)
         if response.status_code == 200:
             dataset = xr.load_dataset(io.BytesIO(response.content), engine='h5netcdf')
@@ -33,9 +31,10 @@ def download_forcings(date_str, forecast_start_hour, comids, sleeptime=0.1):
     return inputs    
 
 def get_forcing_directory():
+    baseurl = 'https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod'
     regex = re.compile('^nwm\.\d{8}/$')
     response = requests.get(baseurl)
-    doc = BeautifulSoup(response.text)
+    doc = BeautifulSoup(response.text, 'lxml')
     body = doc.find(name='body')
     links = body.find_all(name='a')
     urls = []
