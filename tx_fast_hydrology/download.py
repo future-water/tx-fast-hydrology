029cc8998642086da61204909b06df464f0832e9
Matt Bartos <mdbartos@utexas.edu>
2024-11-01 02:42:47 -0500

    Fix NWM download script

--- tx_fast_hydrology/download.py
+++ tx_fast_hydrology/download.py
@@ -1,4 +1,7 @@
+import os
 import time
+import datetime
+import pathlib
 import io
 import re
 import requests
@@ -6,47 +9,64 @@ import xarray as xr
 import pandas as pd
 from bs4 import BeautifulSoup
 
-def get_forcing_directory():
+def get_forcing_directories():
     baseurl = 'https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod'
     regex = re.compile('^nwm\.\d{8}/$')
     response = requests.get(baseurl)
-    doc = BeautifulSoup(response.text, 'lxml')
-    body = doc.find(name='body')
-    links = body.find_all(name='a')
-    urls = []
-    for link in links:
-        if 'href' in link.attrs:
-            url = link.attrs['href']
-            match = regex.match(url)
-            if match:
-                urls.append(url)
-    url = max(urls)
-    return f'{baseurl}/{url}'
+    if response.status_code == 200:
+        doc = BeautifulSoup(response.text, 'lxml')
+        body = doc.find(name='body')
+        links = body.find_all(name='a')
+        urls = []
+        for link in links:
+            if 'href' in link.attrs:
+                url = link.attrs['href']
+                match = regex.match(url)
+                if match:
+                    urls.append(url)
+        return [f'{baseurl}/{url}' for url in urls]
+    else:
+        return []
 
 def get_latest_forecast_hour(nwm_dir):
-    forecast_start_hour = 0
     regex = re.compile('^nwm\.t(\d{2})z\.short_range.channel_rt')
     response = requests.get(f'{nwm_dir}/short_range/')
-    doc = BeautifulSoup(response.content, 'lxml')
-    body = doc.find(name='body')
-    links = body.find_all(name='a')
-    for link in links:
-        if 'href' in link.attrs:
-            url = link.attrs['href']
-            match = regex.match(url)
-            if match:
-                next_hour = int(match.group(1))
-                forecast_start_hour = max(forecast_start_hour, next_hour)
-    return forecast_start_hour
+    if response.status_code == 200:
+        doc = BeautifulSoup(response.content, 'lxml')
+        body = doc.find(name='body')
+        links = body.find_all(name='a')
+        forecast_start_hours = set()
+        for link in links:
+            if 'href' in link.attrs:
+                url = link.attrs['href']
+                match = regex.match(url)
+                if match:
+                    next_hour = int(match.group(1))
+                    forecast_start_hours.add(next_hour)
+        return max(forecast_start_hours)
+    else:
+        return None
 
-def download_forcings(nwm_dir, forecast_start_hour, comids, sleeptime=0.1):
+def get_forecast_path(nwm_dirs):
+    for url in nwm_dirs:
+        forecast_hour = get_latest_forecast_hour(url)
+        if forecast_hour is None:
+            continue
+    return url, forecast_hour
+
+
+def download_forcings(nwm_dir, forecast_hour, comids, sleeptime=0.1):
     # Download NetCDF forcings
     datasets = {}
     for lookahead_hour in range(1, 19):
-        url = f'{nwm_dir}/short_range/nwm.t{forecast_start_hour:02}z.short_range.channel_rt.f{lookahead_hour:03}.conus.nc'
+        nc_url = f'short_range/nwm.t{forecast_hour:02}z.short_range.channel_rt.f{lookahead_hour:03}.conus.nc'
+        url = os.path.join(nwm_dir, nc_url)
+        print(url)
         response = requests.get(url)
         if response.status_code == 200:
             dataset = xr.load_dataset(io.BytesIO(response.content), engine='h5netcdf')
+        else:
+            raise PermissionError(response.status_code)
         datasets[lookahead_hour] = dataset
         time.sleep(sleeptime)
     # Parse NetCDF forcings
